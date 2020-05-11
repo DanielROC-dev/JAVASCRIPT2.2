06541ddeba9f12cf62ba0ceeb49470e3fb0ea653
DanielROC-dev <dkempers@roc-dev.com>
2020-05-11 04:21:41 +0200

    2.2

---
+++ stijl.js
@@ -0,0 +1,3 @@
+document.getElementById("bl1").style.backgroundColor = "lightblue";
+document.getElementById("bl2").style.backgroundColor = "darkgreen";
+document.getElementById("bl4").style.backgroundColor = "red";
